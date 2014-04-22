f18bc978c0a33a3f140a6dce4dcc66ef9e9cc565
Krishnan Mani <kmani@equalexperts.com>
2014-04-23 03:33:36 +0530

    Added a preliminary grinder test script and grinder.properties file for grinder agents

---
+++ testing/grinder/grinder.py
@@ -0,0 +1,14 @@
+# A simple example using the HTTP plugin that shows the retrieval of a
+# single page via HTTP.
+ 
+from net.grinder.script import Test
+from net.grinder.script.Grinder import grinder
+from net.grinder.plugin.http import HTTPRequest
+ 
+test1 = Test(1, "Request resource")
+request1 = HTTPRequest()
+test1.record(request1)
+ 
+class TestRunner:
+    def __call__(self):
+        result = request1.GET("http://ysrtc.co.in:8080/ysrtc/bookTicket?bookedBy=abc")
