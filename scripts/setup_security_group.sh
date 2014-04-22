2c642be1faf6b6ea571acc6a91168fbf7a16a1e1
Krishnan Mani <kmani@equalexperts.com>
2014-04-22 09:19:24 +0530

    Adding scripts that use the aws cli for common tasks

---
+++ scripts/setup_security_group.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REGION=ap-southeast-1
+SECURITY_GROUP_NAME=$1
+DESCRIPTION=$2
+MY_IP_ADDRESS=$3
+
+# Create security group by name
+aws ec2 create-security-group --group-name ${SECURITY_GROUP_NAME} --description ${DESCRIPTION}
+
+# Authorize security group ingress
+## for ssh from this IP address
+aws ec2 authorize-security-group-ingress --region ${REGION} --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_IP_ADDRESS}/32
+
+# List security group created
+aws ec2 describe-security-groups --group-names ${SECURITY_GROUP_NAME}
+
+# Delete security group (ensure to run after any instances that use this security group have been terminated)
+aws ec2 delete-security-group --region ${REGION} --group-name ${SECURITY_GROUP_NAME}
